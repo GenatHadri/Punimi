072abca38131afd1606c365e10794bf5f09251e2
Genat Hadri <gh43836@ubt-uni.net>
2020-05-07 22:04:13 +0200

    java6 ushtrime

---
+++ Java 6 ushtrime/main.js
@@ -0,0 +1,12 @@
+var createDiv = document.createElement('div');
+var createImg = document.createElement('img');
+var createH3 = document.createElement('h3');
+var createP = document.createElement('p');
+var createH6 = document.createElement('h6');
+
+createDiv.appendChild(teksti);
+
+lajmi.titulli = document.getElementById('titulli-new').value;
+var createtitletext = document.createTextNode (lajmi.titulli);
+
+
